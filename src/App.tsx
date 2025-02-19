9dcfcb5cae8a33a930c7a8ae9beaf9d8b563b1a9
Louie <louiemeeks@icloud.com>
2025-02-19 02:34:33 +0000

    started UI design and using mobile first approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <h3>Dungeon Decipherer</h3>
+        <p>Sundered Doctorine</p>
       </header>
+      <div className="content">
+        <div className="box">
+          <h2 className="title">Input</h2>
+          <div className="circle-box">
+            <div className="circle" />
+          </div>
+        </div>
+        <div className="box">
+          <h2 className="title">Solution</h2>
+          <div className="circle-box">
+            <div className="circle" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
